339889950c5c60cb73b4bfe83128a239313adaa8
ivanc <ivancototkd@gmail.com>
2022-03-22 09:35:36 -0600

    Commit number 1, i had created the web site.

---
+++ src/main/java/com/CV_IvanCoto/CvIvanCotoApplication.java
@@ -0,0 +1,13 @@
+package com.CV_IvanCoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvIvanCotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvIvanCotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CV_IvanCoto/CvIvanCotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CV_IvanCoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvIvanCotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
